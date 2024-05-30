1369763dfaca81d065b83ec958ea89f5c7a533b0
AugustinPech <80385451+AugustinPech@users.noreply.github.com>
2024-05-30 10:00:54 +0200

    tutorial finished

--- bottle_app.py
+++ bottle_app.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template
-from bottle import default_app, route
-
+from flask import Flask
+from bottle import default_app, route, static_file
 import os
+
+
 @route('/')
 def hello_world():
     
-    return render_template(os.path.join(os.path.dirname(__file__), 'src','views','index.html'))
+    return static_file('index.html',os.path.join(os.path.dirname(__file__), 'src','views'))
 
 application = default_app()
 
