3a6f8fa9071a13e9732c2b0f548d756c4439948a
AugustinPech <augustin.pech.alcoba@gmail.com>
2024-05-30 07:55:16 +0000

    trash

--- bottle_app.py
+++ bottle_app.py
@@ -4,7 +4,7 @@ from bottle import default_app, route
 import os
 @route('/')
 def hello_world():
-    
+
     return render_template(os.path.join(os.path.dirname(__file__), 'src','views','index.html'))
 
 application = default_app()
