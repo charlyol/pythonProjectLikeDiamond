080c505e5269cd4654ef84f7baea721588f7cebf
AugustinPech <augustin.pech.alcoba@gmail.com>
2024-05-30 08:10:47 +0000

    bottle

--- bottle_app.py
+++ bottle_app.py
@@ -5,13 +5,9 @@ import os
 
 @route('/')
 def hello_world():
-<<<<<<< HEAD
 
-    return static_file('index.html',os.path.join(os.path.dirname(__file__), 'src','views'))
-=======
-    
     return static_file('index.html',root=os.path.join(os.path.dirname(__file__), 'src','views'))
->>>>>>> c1db2041de877df89c4eb78d0a5474d8dc1d4f4b
+
 
 application = default_app()
 
